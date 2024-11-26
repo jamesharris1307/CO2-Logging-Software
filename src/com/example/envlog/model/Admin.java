2374f5c267b9c7204e2c48b53c7ad9c362afea35
James Harris <jamesharris1307@outlook.com>
2024-11-26 14:44:39 +0000

    Constructor added - Admin.java and DataAnalyst.java

--- src/com/example/envlog/model/Admin.java
+++ src/com/example/envlog/model/Admin.java
@@ -2,5 +2,7 @@ package com.example.envlog.model;
 
 public class Admin extends UserInfo{
 
-
+    public Admin(String name, String username, String password) {
+        super(name, username, password);
+    }
 }
--- src/com/example/envlog/model/DataAnalyst.java
+++ src/com/example/envlog/model/DataAnalyst.java
@@ -1,4 +1,8 @@
 package com.example.envlog.model;
 
 public class DataAnalyst extends UserInfo{
+
+    public DataAnalyst(String name, String username, String password) {
+        super(name, username, password);
+    }
 }
