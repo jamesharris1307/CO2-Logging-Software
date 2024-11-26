76b08164b84a913025a5399499b590a290330477
James Harris <jamesharris1307@outlook.com>
2024-11-26 14:33:28 +0000

    Project V1 - Initial Commit

--- src/com/example/envlog/server/Server.java
+++ src/com/example/envlog/server/Server.java
@@ -1,13 +1,9 @@
 package com.example.envlog.server;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.Scanner;
-public class Server1 {
+
+public class Server {
         public static void main(String[] args) throws IOException {
 //           Initialising server socket
             ServerSocket serverSocket = null;
