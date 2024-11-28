42a4c34fa7e31155a3527bb687f7b0f819286498
James Harris <jamesharris1307@outlook.com>
2024-11-28 00:45:45 +0000

    Write to csv file added

--- src/com/example/envlog/util/Tools.java
+++ src/com/example/envlog/util/Tools.java
@@ -4,19 +4,23 @@ package com.example.envlog.util;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Tools {
 
     // Method to Generate Current Timestamp
-    public static String generateTimeStamp() {
-        LocalDateTime now = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
-        return now.format(formatter);
-    } // End Generate Timestamp
+    public static class TimestampGenerator {
+        public static String generateTimestamp() {
+            LocalDateTime now = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+            return now.format(formatter);
+        }
 
-    // Get Timestamp Value
-    public String getTimestamp() {
-        return generateTimeStamp();
+        public static String generateTimestampFormatted() {
+            LocalDateTime now = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("mm:HH:dd:MM:yyyy");
+            return now.format(formatter);
+        }
     }
 
     // Create a Unique User ID
@@ -24,16 +28,32 @@ public class Tools {
         private static Random random = new Random();
 
         public static String generateUniqueUserID() {
-            String date = generateTimeStamp();
+            String date = TimestampGenerator.generateTimestamp();
             int randomNumber = random.nextInt(9000) + 1000;
-
             return String.format("R%s%04d", date, randomNumber);
         }
     }
 
+    public static class UserInputDate {
+        public static String userInputData() {
+            Scanner reader = new Scanner(System.in);
+
+            System.out.println("Input UserID: ");
+            String userIDInput = reader.nextLine();
+            System.out.println("Input Postcode: ");
+            String inputPostcode = reader.nextLine();
+            System.out.println("Input CO2 Reading: ");
+            float co2Reading = reader.nextFloat();
+
+            return userIDInput + inputPostcode + co2Reading;
+        }
+
+    }
+
     // Test Methods
     public static void main(String[] args) {
-        System.out.println(generateTimeStamp());
+        System.out.println(TimestampGenerator.generateTimestamp());
+        System.out.println(TimestampGenerator.generateTimestampFormatted());
         System.out.println(UniqueUserIDGenerator.generateUniqueUserID());
         System.out.println(UniqueUserIDGenerator.generateUniqueUserID());
     }
--- src/com/example/envlog/util/WriteCSV.java
+++ src/com/example/envlog/util/WriteCSV.java
@@ -1,5 +1,4 @@
 package com.example.envlog.util;
-
 import com.example.envlog.model.UserInfo;
 
 import java.io.BufferedWriter;
@@ -37,6 +36,41 @@ public class WriteCSV {
             System.out.println("Couldn't write to file");
         }
 
+    } // End writeUserInfoCSV Method
+
+    // Method to Write to Data CSV File
+    public static void writeDataCSV(){
+
+        // Path to the CSV File
+        String dataCsvFilePath = "src/resources/CO2Data.csv";
+        File dataInfoCSV = new File(dataCsvFilePath);
+        String timeStamp = Tools.TimestampGenerator.generateTimestampFormatted();
+
+        // Create Buffered Writer to write User Input to the CSV file
+        try { BufferedWriter writer = new BufferedWriter(new FileWriter(dataCsvFilePath, true));
+
+            // Write Header Row to the CSV File
+            if (dataInfoCSV.length() == 0) {
+                writer.write("Timestamp(Minute:Hour:Day:Month:Year), UserID, Postcode, Co2 Reading");
+            }
+
+            // Write Data Row to the CSV File
+            writer.newLine();
+            writer.write(timeStamp + "," + "userIDInput" + "," + "inputPostcode" + "," + "co2Reading");
+            writer.close();
+
+            //Success Message
+            System.out.println("Input Successful");
+
+            // Error Handling
+        } catch (IOException ioe) {
+            System.out.println("Couldn't write to file");
+        }
 
     } // End writeUserInfoCSV Method
+
+    public static void main(String[] args) {
+        writeDataCSV();
+    }
+
 }
