f669b06cd4f2612c8f1f665bbcff396f538608f9
james <jamesharris1307@outlook.com>
2024-12-01 17:41:11 +0000

    Merge

--- src/com/example/envlog/util/WriteCSV.java
+++ src/com/example/envlog/util/WriteCSV.java
@@ -1,5 +1,4 @@
 package com.example.envlog.util;
-
 import com.example.envlog.model.UserInfo;
 
 import java.io.BufferedWriter;
@@ -13,19 +12,20 @@ public class WriteCSV {
 
         // Path to the CSV File
         String userCsvFilePath = "src/resources/UserInfo.csv";
-        File userInfoCSV = new File(userCsvFilePath); // Check if the File Exists
+        File userInfoCSV = new File(userCsvFilePath);
+        String userUniqueID = Tools.UniqueUserIDGenerator.generateUniqueUserID();
 
         // Create Buffered Writer to write User Input to the CSV file
         try { BufferedWriter writer = new BufferedWriter(new FileWriter(userCsvFilePath, true));
 
             // Write Header Row to the CSV File
             if (userInfoCSV.length() == 0) {
-                writer.write("Name, Username, Password");
+                writer.write("UserID, Name, Username, Password");
             }
 
             // Write Data Row to the CSV File
             writer.newLine();
-            writer.write(userInfo.getName() + "," + userInfo.getUsername() + "," + userInfo.getPassword() + ",");
+            writer.write( userUniqueID + "," + userInfo.getName() + "," + userInfo.getUsername() + "," + userInfo.getPassword() + ",");
             writer.close();
 
             //Success Message
@@ -37,4 +37,40 @@ public class WriteCSV {
         }
 
     } // End writeUserInfoCSV Method
+
+    // Method to Write to Data CSV File
+    public static void writeDataCSV(){
+
+        // Path to the CSV File
+        String dataCsvFilePath = "src/resources/CO2Data.csv";
+        File dataInfoCSV = new File(dataCsvFilePath);
+        String timeStamp = Tools.TimestampGenerator.generateTimestampFormatted();
+
+        // Create Buffered Writer to write User Input to the CSV file
+        try { BufferedWriter writer = new BufferedWriter(new FileWriter(dataCsvFilePath, true));
+
+            // Write Header Row to the CSV File
+            if (dataInfoCSV.length() == 0) {
+                writer.write("Timestamp(Minute:Hour:Day:Month:Year), UserID, Postcode, Co2 Reading");
+            }
+
+            // Write Data Row to the CSV File
+            writer.newLine();
+            writer.write(timeStamp + "," + "userIDInput" + "," + "inputPostcode" + "," + "co2Reading");
+            writer.close();
+
+            //Success Message
+            System.out.println("Input Successful");
+
+            // Error Handling
+        } catch (IOException ioe) {
+            System.out.println("Couldn't write to file");
+        }
+
+    } // End writeUserInfoCSV Method
+
+    public static void main(String[] args) {
+        writeDataCSV();
+    }
+
 }
