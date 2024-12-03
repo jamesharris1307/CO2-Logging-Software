eb9d6ab9c1a64f0117dbdc5e4a2dc1bb1819ecae
Rymney34 <locktimil70@gmail.com>
2024-12-03 14:42:27 +0000

    main logic is finished

--- src/com/example/envlog/client/Client.java
+++ src/com/example/envlog/client/Client.java
@@ -11,9 +11,9 @@ import java.util.Scanner;
 public class Client {
     public static void client1() throws IOException {
 //       initialising variables
-        Socket server;
-        PrintWriter serverOut;
-        BufferedReader serverIn;
+        Socket server = null;
+        PrintWriter serverOut = null;
+        BufferedReader serverIn = null;
 
         Tools tools1 = new Tools();
 
@@ -21,7 +21,7 @@ public class Client {
 
         Scanner scn = new Scanner(System.in);
 
-//      initialising port number variables
+//      initialising port number variabless
         int portNum;
         int actualPortNum = 1234;
 
@@ -54,37 +54,44 @@ public class Client {
             }
         }
 
+
         BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+
 
         String userInput;
 
         WriteCSV wrt = new WriteCSV();
 
+
         String userID = Login.userId;
 
-        do {
-//            userInp.userInputData();
-//            wrt.writeDataCSV();
-            serverOut.println(userID);
+        while(true) {
+            System.out.println("Would you like to add Record or Close connection: Enter 1 or 2 \n 1) Add record \n 2) Close connection ");
+            int num = scn.nextInt();
 
-            System.out.println("Please enter postcode");
-            userInput = stdIn.readLine();
-            serverOut.println(userInput);
-            System.out.println("Server: " + serverIn.readLine());
+            if (num == 1) {
+                serverOut.println(userID);
 
-            System.out.println("Please enter CO2");
-            userInput = stdIn.readLine();
-            serverOut.println(userInput);
-            System.out.println("Server: " + serverIn.readLine());
-            break;
+                System.out.println("Please enter postcode");
+                userInput = stdIn.readLine();
+                serverOut.println(userInput);
+                System.out.println("Server: " + serverIn.readLine());
+
+                System.out.println("Please enter CO2");
+                userInput = stdIn.readLine();
+                serverOut.println(userInput);
+                System.out.println("Server: " + serverIn.readLine());
+
+            } else {
+                // close, client execution should be stopped
+                serverOut.close();
+                serverIn.close();
+                server.close();
+                break;
+            }
         }
 
-//        userInput = stdIn.readLine();
-//        if user write close, client execution should be stopped
-        while (!userInput.equals ("close"));
 
-        serverOut.close();
-        serverIn.close();
-        server.close();
+
     }
 }
