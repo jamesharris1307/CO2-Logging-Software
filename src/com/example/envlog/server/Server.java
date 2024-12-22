327cb1fbdfb1633662a9a21e3da7b0a4a1fec17c
james <jamesharris1307@outlook.com>
2024-12-22 10:56:55 +0000

    Added the userID to the correct classes

--- src/com/example/envlog/server/Server.java
+++ src/com/example/envlog/server/Server.java
@@ -16,7 +16,7 @@ public class Server {
             }
             catch (IOException e)
             {
-                System.err.println("Could not listen on port: 1111.");
+                System.err.println("Could not listen on port: 2222.");
                 System.exit(-1);
             }
 
